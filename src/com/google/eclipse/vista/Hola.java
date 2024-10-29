eb547d479f6fe6841afd7f9a75b002e0850a15ea
Aaron <aquelat1409@g.educaand.es>
2024-10-29 09:56:58 +0100

    Fusion

--- src/com/google/eclipse/vista/Hola.java
+++ src/com/google/eclipse/vista/Hola.java
@@ -4,11 +4,11 @@ public class Hola {
 public static void main(String[] args) {
 	System.out.println("Hola Mundo!!!!");
 	
-<<<<<<< HEAD
+
 	System.out.println("Estoy bien, gracias, ¿y tú?");
-=======
+
 	System.out.println("¿Qué tal estás?");
->>>>>>> refs/heads/version1
+
 }
 		
 
