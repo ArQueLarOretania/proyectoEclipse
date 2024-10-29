251be128bbd28555d44e5afb3bc93e765a565593
Aaron <aquelat1409@g.educaand.es>
2024-10-29 09:46:23 +0100

    Cambios en version 1

--- src/com/google/eclipse/vista/Hola.java
+++ src/com/google/eclipse/vista/Hola.java
@@ -3,7 +3,9 @@ package com.google.eclipse.vista;
 public class Hola {
 public static void main(String[] args) {
 	System.out.println("Hola Mundo!!!!");
+	
+	System.out.println("¿Qué tal estás?");
 }
-		// TODO Auto-generated method stub
+		
 
 }
