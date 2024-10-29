fb9abcf10f53db832de0b4879edc63ab829c553e
Aaron <aquelat1409@g.educaand.es>
2024-10-29 09:47:57 +0100

    Cambios en Master

--- src/com/google/eclipse/vista/Hola.java
+++ src/com/google/eclipse/vista/Hola.java
@@ -3,6 +3,8 @@ package com.google.eclipse.vista;
 public class Hola {
 public static void main(String[] args) {
 	System.out.println("Hola Mundo!!!!");
+	
+	System.out.println("Estoy bien, gracias, ¿y tú?");
 }
 		// TODO Auto-generated method stub
 
