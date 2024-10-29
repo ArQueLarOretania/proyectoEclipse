12a14126c64a91513d1bbdb2c5bb2f45095a4211
Aaron <aquelat1409@g.educaand.es>
2024-10-29 11:00:51 +0100

    .

--- src/com/google/eclipse/vista/Hola.java
+++ src/com/google/eclipse/vista/Hola.java
@@ -4,19 +4,19 @@ public class Hola {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo!!!!");
 	
-<<<<<<< HEAD
+
 
 	System.out.println("Estoy bien, gracias, ¿y tú?");
 
 	System.out.println("¿Qué tal estás?");
 
-}
-=======
+
+
 		System.out.println("¿Qué tal estás?");
 		
 		System.out.println("Cambios aquí chulillo");
 	}
->>>>>>> refs/heads/version1
+
 		
 
 }
